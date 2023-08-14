f343302cf3392e9d1ea6d9f59931c90ea8d3ff60
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2023-08-14 15:38:44 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
+	experimental: {
+		assets: true
+	},
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'github-dark-dimmed',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				theme: 'github-dark-dimmed',
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
